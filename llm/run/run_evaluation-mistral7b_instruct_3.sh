82b0c71853f23b40828eda185cd260bb3b42e9f9
Benjamin Ye <benjamin.ye@georgian.io>
2024-07-03 11:16:07 -0400

    custom evals

--- llm/run/run_evaluation-mistral7b_instruct_3.sh
+++ llm/run/run_evaluation-mistral7b_instruct_3.sh
@@ -1,23 +1,23 @@
-db_root_path='./data/dev_databases/'
-data_mode='mini_dev' # dev, train, mini_dev
+db_root_path='./mini_dev_data/dev_databases/'
 diff_json_path='./data/mini_dev_sqlite.json' # _sqlite.json, _mysql.json, _postgresql.json
 # Path where the predicted SQL queries are stored
-predicted_sql_path='./exp_result/turbo_output_kg/'
+predicted_sql_path='./exp_result/custom/'
 
 ground_truth_path='./data/'
-num_cpus=16
+num_cpus=4
 meta_time_out=30.0
+
+
+data_mode='mini_dev' # dev, train, mini_dev
+# Choose the engine to run, e.g. gpt-4, gpt-4-32k, gpt-4-turbo, gpt-35-turbo, GPT35-turbo-instruct
+engine='mistral'
+sql_dialect='SQLite'
+
 mode_gt='gt'
 mode_predict='gpt'
 
-# Choose the engine to run, e.g. gpt-4, gpt-4-32k, gpt-4-turbo, gpt-35-turbo, GPT35-turbo-instruct
-engine='gpt-4-turbo'
 
 
-# Choose the SQL dialect to run, e.g. SQLite, MySQL, PostgreSQL
-# PLEASE NOTE: You have to setup the database information in evaluation_utils.py 
-# if you want to run the evaluation script using MySQL or PostgreSQL
-sql_dialect='SQLite'
 
 echo "starting to compare with knowledge for ex engine: ${engine} sql_dialect: ${sql_dialect}"
 python3 -u ./src/evaluation_ex.py --db_root_path ${db_root_path} --predicted_sql_path ${predicted_sql_path} --data_mode ${data_mode} \
